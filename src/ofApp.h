a0e63b156328bab2710a5c93fe38949bfb72b615
stephan schulz <stephan@antimodular.com>
2017-10-09 11:39:03 -0400

    -made simple gui changes -removed  DEBUG define but enabled some debug drawing

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define DEBUG
+//#define DEBUG
 //#define FIXED_TIME //Sets this to fixed time step mode.
 
 #include "ofMain.h"
@@ -75,59 +75,107 @@ class ofApp : public ofBaseApp{
         ofRectangle phase_3A;
         ofRectangle phase_3B;
 
-        size_t ofApp::stringLength(string & str);
+        size_t stringLength(string & str);
 
 
     public:
 
+    
+    // These control where the barriers between the stages are.
+    ofxFloatSlider pool_y;// = 400; // y coordinate where waterfall transforms into pool.
+    
+    //    ofxFloatSlider scroll_y;// = 800; // y coordinate where pool transforms into scroll.
+    ofxFloatSlider scroll_y_start;
+    ofxFloatSlider scroll_y_end;
+    
+    // The dividers between stages in the pool.
+    ofxFloatSlider pool_y1;// = pool_y*.66 + scroll_y*.33; // 1 third to scroll.
+    ofxFloatSlider pool_y2;// = pool_y*.2 + scroll_y*.8; // 2 thirds to scroll.
+    
+    ofxFloatSlider sentances_per_second;// = .3; //.3;
+    
+    // Amount that a given accelaration is damped for altering meandering direction.
+    ofxFloatSlider meanderingDamping_letters;// = .5;
+    
+    // The constant speed that a pool word will meander at.
+    // This should be faster if the pool is larger.
+    //float meanderingSpeed = 50; // Pixels per second.
+    ofxFloatSlider meanderingSpeed_letters;// = 100;
+    // 50 works for 800 wide pool.
+    // 100 for 1920 wide pool.
+    
+    
+    // -- More meandering Sliders for each phase.
+    ofxFloatSlider meanderingDamping_words;//     =  .5;
+    ofxFloatSlider meanderingSpeed_words;//       = 100;
+    ofxFloatSlider meanderingDamping_sentances;// =  .5;
+    ofxFloatSlider meanderingSpeed_sentances;//   = 100;
+    
+    ofxFloatSlider combine_delay_letters;//   = 1;
+    ofxFloatSlider combine_delay_words;
+    ofxFloatSlider combine_delay_sentances;// = 1;
+    ofxFloatSlider max_scroll_delay;//  = 1;
+    
+    
+    // How much the wind affects the letters.
+    ofxFloatSlider wind_factor;// = 0.01;
+    
+    // Rate at which sentances scroll down the screen in the Stage 3: text_scroll.
+    ofxFloatSlider scrollSpeed;// = 40;
+    
+    ofxFloatSlider poolTurnSpeed;// = .1;
+    
+    ofxFloatSlider gravity;// = 30;// pixels / time / time.
+    ofxFloatSlider terminal_velocity;// = 80;
+    
         // -- Parameters that can be tweaked.
 
 
-        // These control where the barriers between the stages are.
-        ofxFloatSlider pool_y = 400; // y coordinate where waterfall transforms into pool.
-
-        ofxFloatSlider scroll_y_start = 800; // y coordinate where pool transforms into scroll.
-        ofxFloatSlider scroll_y_end = 800; // y coordinate where pool transforms into scroll.
-
-                              // The dividers between stages in the pool.
-        ofxFloatSlider pool_y1 = pool_y*.66 + scroll_y_start*.33; // 1 third to scroll.
-        ofxFloatSlider pool_y2 = pool_y*.2 + scroll_y_start*.8; // 2 thirds to scroll.
-
-        ofxFloatSlider sentances_per_second = .3; //.3;
-
-        // Amount that a given accelaration is damped for altering meandering direction.
-        ofxFloatSlider meanderingDamping_letters = .5;
-
-        // The constant speed that a pool word will meander at.
-        // This should be faster if the pool is larger.
-        //float meanderingSpeed = 50; // Pixels per second.
-        ofxFloatSlider meanderingSpeed_letters = 100;
-        // 50 works for 800 wide pool.
-        // 100 for 1920 wide pool.
-
-
-        // -- More meandering Sliders for each phase.
-        ofxFloatSlider meanderingDamping_words     =  .5;
-        ofxFloatSlider meanderingSpeed_words       = 100;
-        ofxFloatSlider meanderingDamping_sentances =  .5;
-        ofxFloatSlider meanderingSpeed_sentances   = 100;
-
-        ofxFloatSlider combine_delay_letters = 5;
-        ofxFloatSlider combine_delay_words = 1;
-        ofxFloatSlider combine_delay_sentances = 1;
-        ofxFloatSlider max_scroll_delay  = 1;
-
-
-        // How much the wind affects the letters.
-        ofxFloatSlider wind_factor = 0.01;
-
-        // Rate at which sentances scroll down the screen in the Stage 3: text_scroll.
-        ofxFloatSlider scrollSpeed = 40;
-
-        ofxFloatSlider poolTurnSpeed = .1;
-
-        ofxFloatSlider gravity = 30;// pixels / time / time.
-        ofxFloatSlider terminal_velocity = 80;
+//        // These control where the barriers between the stages are.
+//        ofxFloatSlider pool_y = 400; // y coordinate where waterfall transforms into pool.
+//
+//        ofxFloatSlider scroll_y_start = 800; // y coordinate where pool transforms into scroll.
+//        ofxFloatSlider scroll_y_end = 800; // y coordinate where pool transforms into scroll.
+//
+//                              // The dividers between stages in the pool.
+//        ofxFloatSlider pool_y1 = pool_y*.66 + scroll_y_start*.33; // 1 third to scroll.
+//        ofxFloatSlider pool_y2 = pool_y*.2 + scroll_y_start*.8; // 2 thirds to scroll.
+//
+//        ofxFloatSlider sentances_per_second = .3; //.3;
+//
+//        // Amount that a given accelaration is damped for altering meandering direction.
+//        ofxFloatSlider meanderingDamping_letters = .5;
+//
+//        // The constant speed that a pool word will meander at.
+//        // This should be faster if the pool is larger.
+//        //float meanderingSpeed = 50; // Pixels per second.
+//        ofxFloatSlider meanderingSpeed_letters = 100;
+//        // 50 works for 800 wide pool.
+//        // 100 for 1920 wide pool.
+//
+//
+//        // -- More meandering Sliders for each phase.
+//        ofxFloatSlider meanderingDamping_words     =  .5;
+//        ofxFloatSlider meanderingSpeed_words       = 100;
+//        ofxFloatSlider meanderingDamping_sentances =  .5;
+//        ofxFloatSlider meanderingSpeed_sentances   = 100;
+//
+//        ofxFloatSlider combine_delay_letters = 5;
+//        ofxFloatSlider combine_delay_words = 1;
+//        ofxFloatSlider combine_delay_sentances = 1;
+//        ofxFloatSlider max_scroll_delay  = 1;
+//
+//
+//        // How much the wind affects the letters.
+//        ofxFloatSlider wind_factor = 0.01;
+//
+//        // Rate at which sentances scroll down the screen in the Stage 3: text_scroll.
+//        ofxFloatSlider scrollSpeed = 40;
+//
+//        ofxFloatSlider poolTurnSpeed = .1;
+//
+//        ofxFloatSlider gravity = 30;// pixels / time / time.
+//        ofxFloatSlider terminal_velocity = 80;
 
         // -- GUI Sliders.        
 
